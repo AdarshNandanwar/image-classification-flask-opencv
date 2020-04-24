41596db17c6cfb0e8ee09728e6256a81d82d503b
Adarsh Nandanwar <nandanwar.adarsh@gmail.com>
2020-04-24 18:25:10 +0530

    v1 complete

--- app.py
+++ app.py
@@ -58,9 +58,9 @@ def index():
             filename = secure_filename(file.filename)
             target = os.path.join(APP_ROOT, 'static/images/')
             file.save(os.path.join(target+'uploads/', str(new_pic.id)))
-            new_pic.data, is_pedestrian, is_face, is_car = classifyImage(new_pic.id)
-            if is_pedestrian:
-                db.session.add(Tags(image_file=new_pic.id, tag='Pedestrian'))
+            new_pic.data, is_full_body, is_face, is_car = classifyImage(new_pic.id)
+            if is_full_body:
+                db.session.add(Tags(image_file=new_pic.id, tag='Full Body'))
             if is_face:
                 db.session.add(Tags(image_file=new_pic.id, tag='Face'))
             if is_car:
@@ -111,9 +111,9 @@ def update(id):
             os.remove(os.path.join(target+'masked/', str(pic.id)+'.jpg'))
             file.save(os.path.join(target+'uploads/', str(pic.id)))
             Tags.query.filter_by(image_file = pic.id).delete()
-            pic.data, is_pedestrian, is_face, is_car = classifyImage(pic.id)
-            if is_pedestrian:
-                db.session.add(Tags(image_file=pic.id, tag='Pedestrian'))
+            pic.data, is_full_body, is_face, is_car = classifyImage(pic.id)
+            if is_full_body:
+                db.session.add(Tags(image_file=pic.id, tag='Full Body'))
             if is_face:
                 db.session.add(Tags(image_file=pic.id, tag='Face'))
             if is_car:
@@ -156,13 +156,14 @@ def classifyImage(id):
 
     target = os.path.join(APP_ROOT, 'static/images/')
     img = cv2.imread(target+'uploads/'+str(id))
+    img = cv2.resize(img, (400, 400))
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # FULL BODY PEDESTRIANS
+    # FULL BODY
     bodyCascade = cv2.CascadeClassifier(os.path.join(APP_ROOT, 'static/cascade/haarcascade_fullbody.xml'))
-    bodies = bodyCascade.detectMultiScale(imgGray, 1.1, 4)
+    bodies = bodyCascade.detectMultiScale(imgGray, 1.1, 6)
     for (x, y, w, h) in bodies:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 3)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
     # FACES
     faceCascade = cv2.CascadeClassifier(os.path.join(APP_ROOT, 'static/cascade/haarcascade_frontalface_default.xml'))
     faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
@@ -170,14 +171,14 @@ def classifyImage(id):
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     # CARS
     carCascade = cv2.CascadeClassifier(os.path.join(APP_ROOT, 'static/cascade/haarcascade_car.xml'))
-    cars = carCascade.detectMultiScale(imgGray, 1.1, 4)
+    cars = carCascade.detectMultiScale(imgGray, 1.1, 6)
     for (x, y, w, h) in cars:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         
     cv2.imwrite(target+'masked/'+str(id)+'.jpg', img) 
-    # return format: data, is_pedestrian, is_face, is_car
-    return "Pedestrians found: "+str(len(bodies))+", Faces found: "+str(len(faces))+", Cars found: "+str(len(cars)), len(bodies)!=0, len(faces)!=0, len(cars)!=0 
+    # return format: data, is_full_body, is_face, is_car
+    return "Full bodies found: "+str(len(bodies))+", Faces found: "+str(len(faces))+", Cars found: "+str(len(cars)), len(bodies)!=0, len(faces)!=0, len(cars)!=0 
 
 
 
